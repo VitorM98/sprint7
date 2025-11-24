3d2fcc02e89397c938b87dc6dcef84876d36081c
VitorM98 <v-ml13@hotmail.com>
2025-11-24 16:57:52 -0300

    alterações

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,18 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  toggleMenu(): void {
-    const menu = document.getElementById('menuModal');
-    const btn = document.getElementById('btn-menu');
-    const overlay = document.getElementById('overlay');
-
-    menu?.classList.toggle('ativo');
-    btn?.classList.toggle('ativo');
-    overlay?.classList.toggle('ativo');
-  }
-
-  fecharBoasVindas(): void {
-    const caixa = document.getElementById('boasVindas');
-    if (caixa) caixa.style.display = 'none';
-  }
+ 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ toggleMenu(): void {
+    const menu = document.getElementById('menuModal');
+    const btn = document.getElementById('btn-menu');
+    const overlay = document.getElementById('overlay');
 
+    menu?.classList.toggle('ativo');
+    btn?.classList.toggle('ativo');
+    overlay?.classList.toggle('ativo');
+  }
+
+  fecharBoasVindas(): void {
+    const caixa = document.getElementById('boasVindas');
+    if (caixa) caixa.style.display = 'none';
+  }
 }
