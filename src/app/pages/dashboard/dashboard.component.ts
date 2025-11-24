9301dbbfe98fafb189badeb3f7c505ae6588e9d6
VitorM98 <v-ml13@hotmail.com>
2025-11-24 16:53:25 -0300

    adição da página home

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  toggleMenu(): void {
+    const menu = document.getElementById('menuModal');
+    const btn = document.getElementById('btn-menu');
+    const overlay = document.getElementById('overlay');
+
+    menu?.classList.toggle('ativo');
+    btn?.classList.toggle('ativo');
+    overlay?.classList.toggle('ativo');
+  }
+
+  fecharBoasVindas(): void {
+    const caixa = document.getElementById('boasVindas');
+    if (caixa) caixa.style.display = 'none';
+  }
+}
